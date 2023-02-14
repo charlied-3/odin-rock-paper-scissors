08aa7540463aa57fc2893111ad3fb3693bafa9c2
Charles D <121022403+charlied-3@users.noreply.github.com>
2023-02-14 17:50:52 -0500

    Complete playRound function

--- play.js
+++ play.js
@@ -1,11 +1,33 @@
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
     if(num === 0) {
-        return("Rock");
+        return("rock");
     } else if(num === 1) {
-        return("Paper");
+        return("paper");
     } else {
-        return("Sissors");
+        return("scissors");
     }
 }
-console.log(getComputerChoice());
+
+function capitalize(choice){
+    return choice.charAt(0).toUpperCase() + choice.slice(1)
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if(playerSelection.toLowerCase() === computerSelection) {
+        return `It's a tie!`
+    } else if((computerSelection === "scissors" && playerSelection === "rock") || 
+    (computerSelection === "rock" && playerSelection === "paper") ||
+    (computerSelection === "paper" && playerSelection === "scissors")) {
+        return `You win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`
+    } else {
+        return `You lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`
+    }
+}
+
+const playerSelection = "ScISSors"
+
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
